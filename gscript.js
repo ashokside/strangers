83b423ed5d4337c7402ca3391227325ecb95bdad
ashokside <ashokside@gmail.com>
2024-09-20 16:47:36 +0530

    file added

--- gscript.js
+++ gscript.js
@@ -7,7 +7,7 @@ function signIn(){
 
     let params ={
         "client_id":"405041029168-t9tge5ajc2ocmgidf761gu4j6330ltvf.apps.googleusercontent.com",
-        "redirect_uri":"http://127.0.0.1:5500/profile.html",
+        "redirect_uri":"https://ashokside.github.io/strangers/profile.html",
         "response_type":"token",
         "scope":"https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/youtube.readonly",
         "include_granted_scopes" : 'true',
